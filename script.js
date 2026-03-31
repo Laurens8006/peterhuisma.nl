3c9f6d81df9f8143c444ea52ae5e62d52b591917
Peter Huisma <phuisma1@st.noorderpoort.nl>
2026-03-31 07:01:46 +0200

    ingeleverd

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
-
-toggleBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
-
